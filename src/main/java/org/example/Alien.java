5164952aaecac36beb8e7005e25dc0a0f6321a06
maaztausif <maaztausif48@gmail.com>
2025-07-03 23:36:28 +0200

    multiple Reference created

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Alien {
+
+    public Alien() {
+        System.out.println("Alien");
+    }
+
     public  void program(){
         System.out.println("Coding...");
     }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,7 +12,11 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien alien = (Alien)context.getBean("alien");
-        System.out.println( "Hello World!");
+//        Laptop lap = new Laptop();
+//        Alien alien = (Alien)context.getBean("alien");
+//        alien.program();
+//
+//        Alien alien1 = (Alien)context.getBean("alien");
+//        alien1.program();
     }
 }
